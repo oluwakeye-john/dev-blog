9426a259ac8436177f407336d346b769169eab9b
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-05-11 23:54:42 +0100

    added prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,4 @@ import '@fortawesome/fontawesome-free/css/brands.min.css'
 import './src/styles/global.scss'
 
 require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
