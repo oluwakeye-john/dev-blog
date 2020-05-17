2414798c8b8e3f6e4966457ab7f2a8fa71cb364c
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-05-17 01:54:59 +0100

    disqus updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import '@fortawesome/fontawesome-free/js/all.min'
 import '@fortawesome/fontawesome-free/css/brands.min.css'
-
+ 
 import './src/styles/global.scss'
 
 require("prismjs/themes/prism-solarizedlight.css")
